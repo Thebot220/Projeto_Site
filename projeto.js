91ded16120ca45e045dbcb0b4521cca75a7c5aa5
Thebot220 <theocosta2021@gmail.com>
2025-09-12 17:38:41 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/projeto.html');
+})
+
+app.listen(port, () => {
+    console.log(`Mensagem envianda ao canal ${port}`);
+})
